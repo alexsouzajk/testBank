af014d53e713ef25ece027c645a982d0b4820829
Alex Jesus <alexsouzaj83@gmail.com>
2024-05-05 02:09:54 -0300

    Commit inicial

---
+++ src/test/java/com/jesus/alex/testBank/TestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.alex.testBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
